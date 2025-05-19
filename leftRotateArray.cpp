86d38f48d713a831a072af095abbf9e372b390ac
sravani-98 <109146581+sravani-98@users.noreply.github.com>
2025-05-19 12:59:54 +0530

    Create leftRotateArray.cpp

---
+++ leftRotateArray.cpp
@@ -0,0 +1,24 @@
+//Left rotate the array by one place
+
+#include <iostream>
+using namespace std;
+
+void leftRotateArr(int arr[], int n){
+  int temp =arr[0];
+  for(int i=1;i<n;i++){
+    arr[i-1] = arr[i];
+  }
+  arr[n-1] = temp;
+
+}
+
+int main(){
+  int arr[] = {1,2,3,4,5};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  leftRotateArr(arr, size);
+  for(int i=0;i<size;i++){
+      cout<<arr[i]<< " ";
+      
+  }
+  cout<<endl;
+}
